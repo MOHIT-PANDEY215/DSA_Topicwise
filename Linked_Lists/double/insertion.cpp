034721b92e8ee4457ce802b51b278566d97e618a
Mohit Pandey <pandeymohit215@gmail.com>
2023-04-17 19:23:46 +0530

    dll

--- Linked_Lists/double/insertion.cpp
+++ Linked_Lists/double/insertion.cpp
@@ -7,41 +7,65 @@ class Node{
     int data;
     Node *next;
     Node *prev;
+
+    // constructor
+    Node(int val){
+        data=val;
+        next=prev=NULL;
+    }
 };
 
-void add_at_end(Node **ref,int new_data){
-    Node *new_node=new Node();
-    new_node -> data =new_data;
+void add_at_end(Node *&ref,int new_data){
+    Node *new_node=new Node(new_data);
+    
     new_node ->next=NULL;
 
-    if(*ref==NULL){
+    if(ref==NULL){
         new_node->prev =NULL;
-        *ref=new_node;
+        ref=new_node;
         return;
     }
-    Node *last =new Node();
+    Node *last=ref;
     while(last->next!=NULL){
         last=last->next;
     }
 
     last->next=new_node;
     new_node->prev=last;
-    return;
 }
 
-void add_at_front(Node **ref,int new_data){
-    Node *new_node=new Node();
-    new_node ->data =new_data;
-    new_node ->next = *ref;
+void add_at_front(Node *&ref,int new_data){
+    Node *new_node=new Node(new_data);
+    
+    new_node ->next = ref;
     new_node ->prev=NULL;
 
-    if(*ref !=NULL)
-    (*ref)->prev=new_node;
+    if(ref !=NULL)
+    ref->prev=new_node;
 
-    *ref=new_node;
+    ref=new_node;
 }
 
-void print(Node *node){
+void add_at_index(Node *&head,int new_data,int index){
+    if(index==1){
+        add_at_front(head,new_data);
+        return;
+    }
+    Node *cur=head,*last;
+    int i=1;
+    while(i!=index){
+        last=cur;
+        cur=cur->next;
+        i++;
+    }
+    Node *temp=new Node(new_data);
+    temp->prev=last;
+    temp->next=last->next;
+    last->next=temp;
+    cur->prev=temp;
+}
+
+void print(Node *&node){
     // Forward print
     cout<<"Forward print\n";
     Node *last; //to store last node of the list
@@ -63,9 +87,14 @@ void print(Node *node){
 
 int main(){
 
-    Node *head=NULL;
-    add_at_end(&head,5);
-    add_at_front(&head,10);
+    Node *head=new Node(1);
+
+    add_at_front(head,10);
+    // 10<->1<->NULL
+    add_at_end(head,5);
+    // 10<->1<->5<->NULL
+    add_at_index(head,7,2);
+    // 10<->7<->1<->5<->NULL
 
     print(head);
     return 0;
